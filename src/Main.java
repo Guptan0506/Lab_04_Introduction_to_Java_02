0cc205701012f72f468230b6a51227972a42ed78
Guptan0506 <navyag0509@gmail.com>
2024-09-21 21:05:03 -0400

    commit

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,8 @@
 public class Main {
     public static void main(String[] args) {
-        ;
+        double priceOfAPurchase = 9.99;
+        double salesTax = 0.05 * priceOfAPurchase;
+        System.out.println("The price of the purchase is "+ priceOfAPurchase + " and the sales tax is "+ salesTax);
+
     }
 }
